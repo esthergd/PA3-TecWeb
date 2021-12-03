becb7308621712b0d2f47522367ab52d246f06c7
BrunoSaboya <brunobs6@al.insper.edu.br>
2021-12-03 17:48:11 -0300

    iniciando as coisas

--- appDietas/views.py
+++ appDietas/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import requests
 
+def diet(request):
+    url = "https://food-nutrition-information.p.rapidapi.com/foods/search"
+    
+    querystring = {"query":"cheese","pageSize":"1","pageNumber":"1"}
 
-def index(request):
-    return HttpResponse("Olá mundo! Este é o app de dietas de Tecnologias Web do Insper.")
+    headers = {
+        'x-rapidapi-host': "food-nutrition-information.p.rapidapi.com",
+        'x-rapidapi-key': "5dc8a618c5msh4ef8167e50b8d14p183fa9jsn573f8dbba635"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    return render(request, 'diet.htm', {'response': response})
